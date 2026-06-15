56438834ad86b1d91fddfc2ca96fac099b8be9bd
Roman Zelenin <rzelenin@gmail.com>
2026-06-15 14:03:15 +0100

    redirect to the index after create\edit item(not global)

--- app/Filament/Resources/Products/Pages/EditProduct.php
+++ app/Filament/Resources/Products/Pages/EditProduct.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Products\Pages;
 use App\Filament\Resources\Products\ProductResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use Override;
 
 class EditProduct extends EditRecord
 {
@@ -15,5 +16,11 @@ class EditProduct extends EditRecord
         return [
             DeleteAction::make(),
         ];
+    }
+
+    #[Override]
+    protected function getRedirectUrl(): string
+    {
+        return ProductResource::getUrl('index');
     }
 }
